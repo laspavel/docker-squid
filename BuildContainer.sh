5dec2c5de2f5b164b5618a47d6dbd6b693854cda
laspavel <laspavel@gmail.com>
2023-04-02 14:51:53 +0300

    Update squid to 5.7-rc0

--- BuildContainer.sh
+++ BuildContainer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CONTAINER_VER=5.2-r0
+CONTAINER_VER=5.7-r0
 docker build --no-cache --file src/Dockerfile --force-rm -t laspavel/squid:$CONTAINER_VER src
 docker tag laspavel/squid:$CONTAINER_VER laspavel/squid:latest
 docker login
